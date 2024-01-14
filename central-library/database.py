ddb95fa5037147a0b7c08d92de1194bfffbd96fb
dejand <dejand@nordeus.com>
2024-01-15 00:14:21 +0100

    Add db start check

--- central-library/database.py
+++ central-library/database.py
@@ -1,15 +1,36 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
 import time
 import os
 # mysql://<username>:<password>@<host>:<port>/<db_name>
 # mysql://root:root@localhost:3306/library
 
-time.sleep(30)
-#env dodaj
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:root@{os.getenv('DATABASE_HOST')}:3306/library"
+def wait_for_db(db_uri):
+    _local_engine = create_engine(db_uri)
 
+    _LocalSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=_local_engine
+    )
+
+    up = False
+    while not up:
+        try:
+            db_session = _LocalSessionLocal()
+            db_session.execute(text("SELECT 1"))
+            db_session.commit()
+        except Exception as err:
+            print(f"Connection error: {err}")
+            up = False
+        else:
+            up = True
+
+        time.sleep(2)
+
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:root@{os.getenv('DATABASE_HOST')}:3306/library"
+wait_for_db(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
--- local-library/database.py
+++ local-library/database.py
@@ -1,30 +1,39 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
 import time
 import os
 # mysql://<username>:<password>@<host>:<port>/<db_name>
 # mysql://root:root@localhost:3306/library
 
-time.sleep(30)
-#env dodaj
-print("Aaaaa")
-print(os.getenv('DATABASE_PORT'))
-try:
-    SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:root@{os.getenv('DATABASE_HOST')}:3306/{os.getenv('LOCAL_DATABASE')}"
-    print(SQLALCHEMY_DATABASE_URL)
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL
-    )
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def wait_for_db(db_uri):
+    _local_engine = create_engine(db_uri)
 
-    Base = declarative_base()
-except:
-    SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:root@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('LOCAL_DATABASE')}"
-    print(SQLALCHEMY_DATABASE_URL)
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL
+    _LocalSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=_local_engine
     )
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    Base = declarative_base()
+    up = False
+    while not up:
+        try:
+            db_session = _LocalSessionLocal()
+            db_session.execute(text("SELECT 1"))
+            db_session.commit()
+        except Exception as err:
+            print(f"Connection error: {err}")
+            up = False
+        else:
+            up = True
+
+        time.sleep(2)
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:root@{os.getenv('DATABASE_HOST')}:3306/{os.getenv('LOCAL_DATABASE')}"
+if os.getenv('DATABASE_HOST') != 'db':
+    wait_for_db(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
