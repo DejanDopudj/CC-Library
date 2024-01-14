fb24fcf275c74ef1f8651be1b19736d39e6b8e89
dejand <dejand@nordeus.com>
2024-01-14 19:47:10 +0100

    Implement central library

--- central-library/crud.py
+++ central-library/crud.py
@@ -2,14 +2,11 @@ from sqlalchemy.orm import Session
 
 from models import User, Item
 from schemas import UserCreate, ItemCreate
+import uuid
 
 
-def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: str):
     return db.query(User).filter(User.id == user_id).first()
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
@@ -17,20 +14,38 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = User(email=user.email, hashed_password=fake_hashed_password)
+    db_user = User(
+        id=user.id,
+        name=user.name,
+        surname=user.surname,
+        address=user.address
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+def get_item(db: Session, id: str):
+    return db.query(Item).filter(Item.id == id).first()
+
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Item).offset(skip).limit(limit).all()
 
+def return_item(db: Session, item_id: str):
+    db.query(Item).filter(Item.id == item_id).update({"returned": True})
+    db.commit()
 
-def create_user_item(db: Session, item: ItemCreate, user_id: int):
-    db_item = Item(**item.dict(), owner_id=user_id)
+def check_items(db: Session, user_id:str):
+    items = db.query(Item).filter(Item.owner_id == user_id, Item.returned == False).count()
+    return items >= 3
+
+def create_user_item(db: Session, item: ItemCreate):
+    db_item = Item(
+        id=uuid.uuid4(),
+        returned=False,
+        owner_id=item.owner_id
+    )
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
